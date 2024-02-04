c0a81763e4e9d81fcc78bd54cedc85c267d28a81
Gera <gr.misterg@gmail.com>
2024-02-03 19:07:09 -0600

    Working on lists

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        <Image ></Image>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const listLang = props => {
+  return (
+    <FlatList
+    data = {[
+      { key : ["Spanish 🇲🇽", "#095f44"]},
+      { key : ["English 🇺🇸", "#cf2335"]},
+      { key : ["French 🇫🇷", "#0a4f8f"]},
+      { key : ["Italian 🇮🇹", "#088042"]},
+    ]}
+    renderItem={
+      ({item}) => {
+        <Text style = {[styles.txt, { color: item.key[1] }]}>{item.key[0]}</Text>
+      }
+    }
+  />
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +37,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical : 60,
   },
+  h1 : {
+    fontSize : 40,
+    fontWeight : "bold",
+  },
+  h2 : {
+    fontSize : 32,
+    fontWeight : "bold",
+  },
+  h3 : {
+    fontSize : 28,
+    fontWeight : "bold",
+  },
+  txt : {
+    fontSize : 28,
+    fontWeight : "normal",
+  }
 });
+
+export default App;
